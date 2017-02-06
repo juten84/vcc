3ad26de2253bec493f2a90bde348686c31169191
juten84 <justin.vailhere@orange.fr>
2017-02-06 10:52:46 +0100

    Bon...

--- src/main/java/fr/unice/vicc/NaiveVmAllocationPolicy.java
+++ src/main/java/fr/unice/vicc/NaiveVmAllocationPolicy.java
@@ -1,26 +1,26 @@
 package fr.unice.vicc;
 
-import org.cloudbus.cloudsim.Host;
-import org.cloudbus.cloudsim.Vm;
-import org.cloudbus.cloudsim.VmAllocationPolicy;
-import org.cloudbus.cloudsim.power.PowerHost;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.VmAllocationPolicy;
 
 /**
  * Created by fhermeni2 on 16/11/2015.
  */
 public class NaiveVmAllocationPolicy extends VmAllocationPolicy {
 
-    /** The map to track the server that host each running VM. */
-    private Map<Vm,Host> hoster;
+    /**
+     * The map to track the server that host each running VM.
+     */
+    private Map<Vm, Host> hoster;
 
     public NaiveVmAllocationPolicy(List<? extends Host> list) {
         super(list);
-        hoster =new HashMap<>();
+        setHostList(list);
     }
 
     @Override
@@ -35,26 +35,42 @@ public class NaiveVmAllocationPolicy extends VmAllocationPolicy {
     }
 
     @Override
-    public boolean allocateHostForVm(Vm vm) {        
+    public boolean allocateHostForVm(Vm vm) {
+        for (Host host : getHostList()) {
+            if (allocateHostForVm(vm, host) == true)
+                return true;
+        }
         return false;
     }
 
     @Override
     public boolean allocateHostForVm(Vm vm, Host host) {
-        return false;
+        if (host.vmCreate(vm) == true) {
+            hoster.put(vm, host);
+            return true;
+        } else
+            return false;
     }
 
     @Override
     public void deallocateHostForVm(Vm vm) {
+        Host host = getHost(vm);
+        host.vmDestroy(vm);
     }
 
     @Override
     public Host getHost(Vm vm) {
-        return null;
+        Host host = hoster.get(vm);
+        return host;
     }
 
     @Override
     public Host getHost(int vmId, int userId) {
+        for (Vm vm : hoster.keySet()) {
+            if (vm.getId() == vmId && vm.getUserId() == userId) {
+                return getHost(vm);
+            }
+        }
         return null;
     }
 }
